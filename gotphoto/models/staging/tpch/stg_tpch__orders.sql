d72261bdfe6b46bf9c176bd78cb4159874ae1396
Sidhartha Tammineni <tamminenisidhartha@gmail.com>
2026-07-18 18:08:50 +0200

    failure check for columns

--- gotphoto/models/staging/tpch/stg_tpch__orders.sql
+++ gotphoto/models/staging/tpch/stg_tpch__orders.sql
@@ -14,7 +14,7 @@ with source as (
 
 renamed as (
     select
-        o_orderkey      as order_id,
+        o_orderkey_fake      as order_id,
         o_custkey       as customer_id,
         o_orderstatus   as order_status,
         o_totalprice    as order_total_price,
